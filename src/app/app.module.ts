4334450c3ff040080137eb879c0078bd2d978667
JuanCMulet <73266014+JuanCMulet@users.noreply.github.com>
2022-01-13 19:26:44 -0300

    create basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
